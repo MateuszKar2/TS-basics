4a9c4d9fe4ac3398f405fbb2069d74190645829f
MateuszKar2 <mateuszwasabi@gmial.com>
2024-10-14 22:12:49 +0200

    My changes

--- index.ts
+++ index.ts
@@ -2,16 +2,33 @@ import axios from 'axios';
 
 const url = 'https://jsonplaceholder.typicode.com/todos/1';
 
+interface Todo {
+  id: number,
+  title: string,
+  completed: boolean,
+};
+
 axios.get(url).then(response => {
-  const todo = response.data;
 
-  const ID = todo.ID;
-  const title = todo.Title;
-  const finished = todo.finished;
+  //dane odpowiedzi mają właściwości 
+  //wymieniliśmy właściwości
+  //TS to dodoanie komentarza w formie maszynopisu
+  const todo = response.data as Todo;
 
-  console.log(`
-    The Todo with ID: ${ID}
-    Has a title of: ${title}
-    Is it finished? ${finished}
-    `);
+  const id = todo.id;
+  const title = todo.title;
+  const completed = todo.completed;
+
+  logTodo(id, title, completed);
+
+}).catch(error => {
+  console.log(error);
 });
+
+const logTodo = (id: number, title: string, completed: boolean) => {
+  console.log(`
+    The Todo with ID: ${id}
+    Has a title of: ${title}
+    Is it completed? ${completed}
+    `);
+}
